8281cb5833ad41c6182085f5b78e56f40b862896
Geoffrey Brunet <geoffrey-brunet89@outlook.fr>
2020-01-08 23:44:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,12 +26,33 @@ resource "scaleway_instance_security_group" "www" {
     action = "accept"
     port   = "443"
   }
+  
+    inbound_rule {
+    action = "accept"
+    port   = "5222"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "9001"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "9050"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "9051"
+  }
 }
 
 resource "scaleway_instance_server" "tor" {
   type  = "DEV1-S"
-  image = "ubuntu-bionic"
-  tags = [ "tor" ]
+  image = "bac6cb94-85db-4fb8-a789-2ad8526be9d2"
+  tags = [ "tor", "relay" ]
   ip_id = scaleway_instance_ip.public_ip.id
+  enable_ipv6 = true
   security_group_id = scaleway_instance_security_group.www.id
 }
